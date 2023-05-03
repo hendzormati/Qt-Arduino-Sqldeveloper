f0fd50d95373db5c585bde827f0a4bf895d5491a
nourlouhaichi <Nour.Louhaichi@esprit.tn>
2023-05-03 20:22:49 +0100

    comitoutet

--- Ahminy/connection.cpp
+++ Ahminy/connection.cpp
@@ -4,15 +4,17 @@ connection::connection()
 {
 }
 bool connection::createconnect()
-{bool test=false;
+{
+    bool test=false;
     db = QSqlDatabase::addDatabase("QODBC");
     db.setDatabaseName("ahminy");
-    db.setUserName("hend");//inserer nom de l'utilisateur
-    db.setPassword("esprit23");//inserer mot de passe de cet utilisateur
+    db.setUserName("louhaichi");
+    db.setPassword("esprit");
     if (db.open())
         test=true;
     return  test;
 }
+
 void connection::closeconnect()
 {
     db.close();
